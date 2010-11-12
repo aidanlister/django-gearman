088fa74caccb504efdeddfa319c05fb7c21f0f58
Aidan Lister <aidan@php.net>
2010-11-12 05:28:00 +0000

    Add support for transparent pickling

--- django_gearman/__init__.py
+++ django_gearman/__init__.py
@@ -1,1 +1,8 @@
-import gearman
+def check_request_status(job_request):
+    """ Debugging function to check the status of a request. """
+    if job_request.complete:
+        print "Job %s finished!  Result: %s - %s" % (job_request.job.unique, job_request.state, job_request.result)
+    elif job_request.timed_out:
+        print "Job %s timed out!" % job_request.unique
+    elif job_request.state == JOB_UNKNOWN:
+        print "Job %s connection failed!" % job_request.unique
--- django_gearman/models.py
+++ django_gearman/models.py
@@ -1,17 +1,32 @@
 from django.conf import settings
-from gearman import GearmanClient, GearmanWorker
+import gearman
+import pickle
 
-class DjangoGearmanClient(GearmanClient):
+class PickleDataEncoder(gearman.DataEncoder):
+    """ Since this is a Django app, allow the passing of raw python objects. """
+    @classmethod
+    def encode(cls, encodable_object):
+        return pickle.dumps(encodable_object)
+
+    @classmethod
+    def decode(cls, decodable_string):
+        return pickle.loads(decodable_string)
+
+
+class DjangoGearmanClient(gearman.GearmanClient):
     """ Gearman client which automatically connects to configured server. """
-
+    data_encoder = PickleDataEncoder
+    
     def __init__(self, **kwargs):
         return super(DjangoGearmanClient, self).__init__(
                 settings.GEARMAN_SERVERS, **kwargs)
 
 
-class DjangoGearmanWorker(GearmanWorker):
+class DjangoGearmanWorker(gearman.GearmanWorker):
     """ Gearman workder which automatically connects to the configured server. """
 
     def __init__(self, **kwargs):
         return super(DjangoGearmanWorker, self).__init__(
                 settings.GEARMAN_SERVERS, **kwargs)
+
+

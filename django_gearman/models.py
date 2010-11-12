08c36a0e9fc0f38ce1ca188edb0cafb5b83d5e0e
Aidan Lister <aidan@php.net>
2010-11-12 03:02:30 +0000

    Clean up commenting

--- django_gearman/models.py
+++ django_gearman/models.py
@@ -1,27 +1,17 @@
 from django.conf import settings
 from gearman import GearmanClient, GearmanWorker
 
-
 class DjangoGearmanClient(GearmanClient):
-    """gearman client, automatically connecting to server"""
-
-    def __call__(self, func, arg, uniq=None, **kwargs):
-        raise NotImplementedError('Use do_task() or dispatch_background'\
-                                  '_task() instead')
+    """ Gearman client which automatically connects to configured server. """
 
     def __init__(self, **kwargs):
-        """instantiate Gearman client with servers from settings file"""
         return super(DjangoGearmanClient, self).__init__(
                 settings.GEARMAN_SERVERS, **kwargs)
 
 
 class DjangoGearmanWorker(GearmanWorker):
-    """
-    gearman worker, automatically connecting to server and discovering
-    available jobs
-    """
+    """ Gearman workder which automatically connects to the configured server. """
 
     def __init__(self, **kwargs):
-        """instantiate Gearman worker with servers from settings file"""
         return super(DjangoGearmanWorker, self).__init__(
                 settings.GEARMAN_SERVERS, **kwargs)

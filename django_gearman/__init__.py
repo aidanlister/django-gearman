89a0e9e75f2e33a2962772cdf1138b9f8eabbdef
Aidan Lister <aidan@php.net>
2010-11-13 02:09:40 +0000

    It doesn't make sense to have non model code in models.py

--- django_gearman/__init__.py
+++ django_gearman/__init__.py
@@ -1,3 +1,20 @@
+import gearman
+from django.conf import settings
+
+class DjangoGearmanClient(gearman.GearmanClient):
+    """ Gearman client which automatically connects to configured server. """
+    def __init__(self, **kwargs):
+        return super(DjangoGearmanClient, self).__init__(
+                settings.GEARMAN_SERVERS, **kwargs)
+
+
+class DjangoGearmanWorker(gearman.GearmanWorker):
+    """ Gearman workder which automatically connects to the configured server. """
+
+    def __init__(self, **kwargs):
+        return super(DjangoGearmanWorker, self).__init__(
+                settings.GEARMAN_SERVERS, **kwargs)
+
 def check_request_status(job_request):
     """ Debugging function to check the status of a request. """
     if job_request.complete:
